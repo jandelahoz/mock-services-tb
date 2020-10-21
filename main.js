eb80587b343f18cadaba2e5c55189f1bd197c5d9
Sandbox Sync <support@getsandbox.com>
2020-10-21 06:23:26 +0000

    Changes

--- main.js
+++ main.js
@@ -114,3 +114,19 @@ Sandbox.soap('/AV_consultarVuelo', 'http://servicios.avianca.com//consultarVuelo
 	    fechaLlegada3 : new Date(newDate.setTime(oldDate.getTime() + (55 * 60 * 1000))).toUTCString()
 	});
 });
+
+Sandbox.soap('/HiltonRoomService','http://xmlns.oracle.com/HiltonRoomService//', function(req, res) {
+    // Check the request, make sure it is a compatible type, covers both SOAP 1.1 and 1.2
+    if (!req.is('text/xml') && !req.is('application/xml') && !req.is('application/soap')) {
+        return res.send(400, 'Invalid content type, expected application/soap+xml');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/soap+xml');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.render('Http_xmlns_oracle_com_HiltonRoomService_');
+})
